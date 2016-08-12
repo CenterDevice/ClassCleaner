31f3de24f85fcbcaa4d1b90e5c6cbf385b430850
Jan Gassen <jan.gassen@centerdevice.com>
2016-08-12 15:50:55 +0200

    Add analyzers to default analysis

--- src/main/java/de/centerdevice/classcleaner/ClassCleanerBuilder.java
+++ src/main/java/de/centerdevice/classcleaner/ClassCleanerBuilder.java
@@ -15,7 +15,9 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 
+import de.centerdevice.classcleaner.core.analyzer.ForeignMethodAnalyzer;
 import de.centerdevice.classcleaner.core.analyzer.MethodClusterAnalyzer;
+import de.centerdevice.classcleaner.core.analyzer.ReferenceCycleAnalyzer;
 import de.centerdevice.classcleaner.core.analyzer.UnusedMethodAnalyzer;
 import de.centerdevice.classcleaner.core.model.Issue;
 import de.centerdevice.classcleaner.core.recon.ReferenceReport;
@@ -100,6 +102,8 @@ public class ClassCleanerBuilder extends IncrementalProjectBuilder {
 		Set<Issue> issues = new HashSet<>();
 		issues.addAll(new UnusedMethodAnalyzer().analyze(report));
 		issues.addAll(new MethodClusterAnalyzer().analyze(report));
+		issues.addAll(new ReferenceCycleAnalyzer().analyze(report));
+		issues.addAll(new ForeignMethodAnalyzer().analyze(report));
 
 		marker.addMarker(resource, issues);
 	}
