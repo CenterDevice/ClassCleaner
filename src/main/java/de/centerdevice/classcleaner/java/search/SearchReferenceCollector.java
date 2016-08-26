2e5467f59c228edf522c8db9419b2c35c2f215e6
Jan Gassen <jan.gassen@centerdevice.com>
2016-08-26 14:38:22 +0200

    Also capture references to fields and nested types

--- src/main/java/de/centerdevice/classcleaner/java/search/SearchReferenceCollector.java
+++ src/main/java/de/centerdevice/classcleaner/java/search/SearchReferenceCollector.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.search.MethodReferenceMatch;
+import org.eclipse.jdt.core.search.ReferenceMatch;
 import org.eclipse.jdt.core.search.SearchMatch;
 import org.eclipse.jdt.core.search.SearchRequestor;
 
@@ -15,16 +15,17 @@ class SearchReferenceCollector extends SearchRequestor {
 
 	@Override
 	public void acceptSearchMatch(SearchMatch match) throws CoreException {
-		if (match instanceof MethodReferenceMatch) {
-			references.add(getJavaElement((MethodReferenceMatch) match));
+		if (match instanceof ReferenceMatch) {
+			references.add(getJavaElement((ReferenceMatch) match));
 		}
+
 	}
 
 	public List<IJavaElement> getReferences() {
 		return references;
 	}
 
-	protected IJavaElement getJavaElement(MethodReferenceMatch match) {
+	protected IJavaElement getJavaElement(ReferenceMatch match) {
 		return (IJavaElement) match.getElement();
 	}
 }
