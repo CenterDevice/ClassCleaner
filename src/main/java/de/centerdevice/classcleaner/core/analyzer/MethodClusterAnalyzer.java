58db4313f08070b0883070878e6f999ab1bf3dc8
Jan Gassen <jan.gassen@centerdevice.com>
2016-09-09 10:53:42 +0200

    Only suggest method extractions if there is more than one method (and n fields)

--- src/main/java/de/centerdevice/classcleaner/core/analyzer/MethodClusterAnalyzer.java
+++ src/main/java/de/centerdevice/classcleaner/core/analyzer/MethodClusterAnalyzer.java
@@ -1,9 +1,11 @@
 package de.centerdevice.classcleaner.core.analyzer;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IMarker;
 
@@ -11,6 +13,7 @@ import de.centerdevice.classcleaner.core.engine.ReferenceGraph;
 import de.centerdevice.classcleaner.core.model.ClassInfo;
 import de.centerdevice.classcleaner.core.model.CodeElement;
 import de.centerdevice.classcleaner.core.model.Issue;
+import de.centerdevice.classcleaner.core.model.MethodElement;
 import de.centerdevice.classcleaner.core.recon.ReferenceReport;
 
 public class MethodClusterAnalyzer implements ReferenceAnalyzer {
@@ -30,7 +33,8 @@ public class MethodClusterAnalyzer implements ReferenceAnalyzer {
 
 	protected List<Issue> analyzeIssuesForClass(ReferenceReport report, ClassInfo classInfo) {
 		ReferenceGraph clustering = report.getReferenceGraph(classInfo);
-		List<Set<CodeElement>> referenceGroups = clustering.getReferenceGroups(classInfo);
+		List<Set<CodeElement>> referenceGroups = getGroupsWithMoreThanNMethods(classInfo, clustering, 1);
+
 		List<Issue> issues = new ArrayList<>();
 		if (referenceGroups.size() > 1) {
 			for (Set<CodeElement> set : referenceGroups) {
@@ -38,6 +42,15 @@ public class MethodClusterAnalyzer implements ReferenceAnalyzer {
 			}
 		}
 		return issues;
+	}
+
+	public List<Set<CodeElement>> getGroupsWithMoreThanNMethods(ClassInfo classInfo, ReferenceGraph clustering, int n) {
+		return clustering.getReferenceGroups(classInfo).stream().filter(group -> getMethodCount(group) > n)
+				.collect(Collectors.toList());
+	}
+
+	protected long getMethodCount(Collection<CodeElement> elements) {
+		return elements.stream().filter(element -> (element instanceof MethodElement)).count();
 	}
 
 	protected String getMessage(Set<CodeElement> set) {
